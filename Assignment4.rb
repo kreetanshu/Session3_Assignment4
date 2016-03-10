677485ebd560c670e359da8c9895fc5ece85d548
reetanshu <kreetanshu@gmail.com>
2016-03-10 15:49:48 +0530

    Assign4

---
+++ Assignment4.rb
@@ -0,0 +1,18 @@
+Quiz Answers
+
+1) Type casting means
+Ans b)Converting one data type into other
+
+2)Global variable can be denoted with
+
+Ans c) $
+
+3)If @ is the starting letter of a variable that will be denoted as
+Ans c) Instance Variable
+
+4)Def, end Keywords can be used in
+
+Ans b) Method
+
+5) The Super class of all class in Ruby is
+c) Object(Basic object class)
